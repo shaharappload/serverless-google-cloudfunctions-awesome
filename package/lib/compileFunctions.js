12b3eb14962c989428d4a73089cde8349749079d
Shahar Porat <shahar@appload.io>
2020-04-05 17:18:46 +0300

    Fix accesscontrol

--- package/lib/compileFunctions.js
+++ package/lib/compileFunctions.js
@@ -1,79 +1,98 @@
-'use strict';
+"use strict";
 
 /* eslint no-use-before-define: 0 */
 
-const path = require('path');
+const path = require("path");
 
-const _ = require('lodash');
-const BbPromise = require('bluebird');
+const _ = require("lodash");
+const BbPromise = require("bluebird");
 
 module.exports = {
   compileFunctions() {
     const artifactFilePath = this.serverless.service.package.artifact;
     const fileName = artifactFilePath.split(path.sep).pop();
 
-    this.serverless.service.package
-      .artifactFilePath = `${this.serverless.service.package.artifactDirectoryName}/${fileName}`;
+    this.serverless.service.package.artifactFilePath = `${this.serverless.service.package.artifactDirectoryName}/${fileName}`;
 
     this.serverless.service.getAllFunctions().forEach((functionName) => {
       const funcObject = this.serverless.service.getFunction(functionName);
 
-      this.serverless.cli
-        .log(`Compiling function "${functionName}"...`);
+      this.serverless.cli.log(`Compiling function "${functionName}"...`);
 
-      funcObject.prependStage = _.get(funcObject, 'prependStage', this.options.prependStage);
-      funcObject.prependService = _.get(funcObject, 'prependService', this.options.prependService);
-      funcObject.prefix = _.get(funcObject, 'prefix', this.options.prefix);
+      funcObject.prependStage = _.get(
+        funcObject,
+        "prependStage",
+        this.options.prependStage
+      );
+      funcObject.prependService = _.get(
+        funcObject,
+        "prependService",
+        this.options.prependService
+      );
+      funcObject.prefix = _.get(funcObject, "prefix", this.options.prefix);
 
       validateHandlerProperty(funcObject, functionName);
       validateEventsProperty(funcObject, functionName);
       const funcTemplate = getFunctionTemplate(
         funcObject,
-        _.get(this, 'serverless.service.provider.region', this.options.region),
+        _.get(this, "serverless.service.provider.region", this.options.region),
         this.options.stage,
         this.serverless.service.service,
         this.serverless.service.provider.project,
-        `gs://${
-          this.serverless.service.provider.deploymentBucketName
-          }/${this.serverless.service.package.artifactFilePath}`,
+        `gs://${this.serverless.service.provider.deploymentBucketName}/${this.serverless.service.package.artifactFilePath}`
       );
+
+      const accessControlTemplate = getAccessControlTemplate();
 
       const eventType = Object.keys(funcObject.events[0])[0];
 
-      funcTemplate.properties.availableMemoryMb = _.get(funcObject, 'memorySize')
-        || _.get(this, 'serverless.service.provider.memorySize')
-        || 256;
-      funcTemplate.properties.runtime = _.get(funcObject, 'runtime')
-        || _.get(this, 'serverless.service.provider.runtime')
-        || 'nodejs8';
-      funcTemplate.properties.timeout = _.get(funcObject, 'timeout')
-        || _.get(this, 'serverless.service.provider.timeout')
-        || '60s';
-      funcTemplate.properties.serviceAccountEmail = _.get(funcObject, 'serviceAccount')
-        || _.get(this, 'serverless.service.provider.serviceAccount')
-        || '';
-      funcTemplate.properties.vpcConnector = _.get(funcObject, 'vpcConnector')
-        || '';
+      funcTemplate.properties.availableMemoryMb =
+        _.get(funcObject, "memorySize") ||
+        _.get(this, "serverless.service.provider.memorySize") ||
+        256;
+      funcTemplate.properties.runtime =
+        _.get(funcObject, "runtime") ||
+        _.get(this, "serverless.service.provider.runtime") ||
+        "nodejs8";
+      funcTemplate.properties.timeout =
+        _.get(funcObject, "timeout") ||
+        _.get(this, "serverless.service.provider.timeout") ||
+        "60s";
+      funcTemplate.properties.serviceAccountEmail =
+        _.get(funcObject, "serviceAccount") ||
+        _.get(this, "serverless.service.provider.serviceAccount") ||
+        "";
+      funcTemplate.properties.vpcConnector =
+        _.get(funcObject, "vpcConnector") || "";
 
-      if (funcTemplate.properties.vpcConnector && funcTemplate.properties.vpcConnector.length > 0) {
-        funcTemplate.properties.vpcConnectorEgressSettings = _.get(funcObject, 'vpcConnectorEgressSettings')
-          || "ALL_TRAFFIC";
+      if (
+        funcTemplate.properties.vpcConnector &&
+        funcTemplate.properties.vpcConnector.length > 0
+      ) {
+        funcTemplate.properties.vpcConnectorEgressSettings =
+          _.get(funcObject, "vpcConnectorEgressSettings") || "ALL_TRAFFIC";
       }
 
-      if (eventType == 'http') {
-        funcTemplate.properties.unauthenticatedAccess = _.get(funcObject, 'unauthenticatedAccess')
-          || false;
-      } 
+      if (
+        eventType == "http" &&
+        funcTemplate.properties.unauthenticatedAccess == true
+      ) {
+        accessControlTemplate.gcpIamPolicy.bindings.push({
+          role: "roles/cloudfunctions.invoker",
+          members: ["allUsers"],
+        });
+      }
 
-      funcTemplate.properties.labels = _.assign({},
-        _.get(this, 'serverless.service.provider.labels') || {},
-        _.get(funcObject, 'labels') || {},
+      funcTemplate.properties.labels = _.assign(
+        {},
+        _.get(this, "serverless.service.provider.labels") || {},
+        _.get(funcObject, "labels") || {}
       );
       funcTemplate.properties.environmentVariables = _.merge(
-        _.get(this, 'serverless.service.provider.environment'),
-        funcObject.environment,
+        _.get(this, "serverless.service.provider.environment"),
+        funcObject.environment
       );
-      
+
       if (!funcTemplate.properties.serviceAccountEmail) {
         delete funcTemplate.properties.serviceAccountEmail;
       }
@@ -86,21 +105,17 @@ module.exports = {
         delete funcTemplate.properties.vpcConnectorEgressSettings;
       }
 
-      if (!funcTemplate.properties.unauthenticatedAccess) {
-        delete funcTemplate.properties.unauthenticatedAccess;
-      }
-
       if (!_.size(funcTemplate.properties.environmentVariables)) {
         delete funcTemplate.properties.environmentVariables;
       }
 
-      if (eventType === 'http') {
+      if (eventType === "http") {
         const url = funcObject.events[0].http;
 
         funcTemplate.properties.httpsTrigger = {};
         funcTemplate.properties.httpsTrigger.url = url;
       }
-      if (eventType === 'event') {
+      if (eventType === "event") {
         const type = funcObject.events[0].event.eventType;
         const path = funcObject.events[0].event.path; //eslint-disable-line
         const resource = funcObject.events[0].event.resource;
@@ -109,11 +124,20 @@ module.exports = {
         funcTemplate.properties.eventTrigger = {};
         funcTemplate.properties.eventTrigger.eventType = type;
         if (path) funcTemplate.properties.eventTrigger.path = path;
-        if (retry) funcTemplate.properties.eventTrigger.failurePolicy = { retry: {} };
+        if (retry)
+          funcTemplate.properties.eventTrigger.failurePolicy = { retry: {} };
         funcTemplate.properties.eventTrigger.resource = resource;
       }
 
-      this.serverless.service.provider.compiledConfigurationTemplate.resources.push(funcTemplate);
+      if (
+        accessControlTemplate.accessControl.gcpIamPolicy.bindings.length > 0
+      ) {
+        funcTemplate = _.assign({}, funcTemplate, accessControlTemplate);
+      }
+
+      this.serverless.service.provider.compiledConfigurationTemplate.resources.push(
+        funcTemplate
+      );
     });
 
     return BbPromise.resolve();
@@ -125,8 +149,8 @@ const validateHandlerProperty = (funcObject, functionName) => {
     const errorMessage = [
       `Missing "handler" property for function "${functionName}".`,
       ' Your function needs a "handler".',
-      ' Please check the docs for more info.',
-    ].join('');
+      " Please check the docs for more info.",
+    ].join("");
     throw new Error(errorMessage);
   }
 };
@@ -136,32 +160,40 @@ const validateEventsProperty = (funcObject, functionName) => {
     const errorMessage = [
       `Missing "events" property for function "${functionName}".`,
       ' Your function needs at least one "event".',
-      ' Please check the docs for more info.',
-    ].join('');
+      " Please check the docs for more info.",
+    ].join("");
     throw new Error(errorMessage);
   }
 
   if (funcObject.events.length > 1) {
     const errorMessage = [
       `The function "${functionName}" has more than one event.`,
-      ' Only one event per function is supported.',
-      ' Please check the docs for more info.',
-    ].join('');
+      " Only one event per function is supported.",
+      " Please check the docs for more info.",
+    ].join("");
     throw new Error(errorMessage);
   }
 
-  const supportedEvents = ['http', 'event'];
+  const supportedEvents = ["http", "event"];
   const eventType = Object.keys(funcObject.events[0])[0];
   if (supportedEvents.indexOf(eventType) === -1) {
     const errorMessage = [
       `Event type "${eventType}" of function "${functionName}" not supported.`,
-      ` supported event types are: ${supportedEvents.join(', ')}`,
-    ].join('');
+      ` supported event types are: ${supportedEvents.join(", ")}`,
+    ].join("");
     throw new Error(errorMessage);
   }
 };
 
-const getFunctionTemplate = (funcObject, region, stage, service, project, sourceArchiveUrl) => { //eslint-disable-line
+const getFunctionTemplate = (
+  funcObject,
+  region,
+  stage,
+  service,
+  project,
+  sourceArchiveUrl
+) => {
+  //eslint-disable-line
   let funcName = funcObject.handler;
 
   if (funcObject.prependStage) {
@@ -172,21 +204,31 @@ const getFunctionTemplate = (funcObject, region, stage, service, project, source
     funcName = `${service}-${funcName}`;
   }
 
-  if (funcObject.prefix && funcObject.prefix !=='') {
+  if (funcObject.prefix && funcObject.prefix !== "") {
     funcName = `${funcObject.prefix}-${funcName}`;
   }
 
   return {
-    type: 'gcp-types/cloudfunctions-v1:projects.locations.functions',
+    type: "gcp-types/cloudfunctions-v1:projects.locations.functions",
     name: funcObject.name,
     properties: {
       parent: `projects/${project}/locations/${region}`,
       availableMemoryMb: 256,
-      runtime: 'nodejs8',
-      timeout: '60s',
+      runtime: "nodejs8",
+      timeout: "60s",
       entryPoint: funcObject.handler,
       function: funcName,
       sourceArchiveUrl,
+    },
+  };
+};
+
+const getAccessControlTemplate = () => {
+  return {
+    accessControl: {
+      gcpIamPolicy: {
+        bindings: [],
+      },
     },
   };
 };
